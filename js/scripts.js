7f8b1e6969f111f59dbdadf031b1c4ab6307f6ca
Student <example@example.com>
2019-10-22 10:09:58 -0700

    2 for 1, add a button practice site with different html, css, and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
+// $(document).ready(function() {
+//   $(".clickable").click(function() {
+//     $(".initially-showing").toggle();
+//     $(".initially-hidden").toggle();
+//     $(".lamprey").toggle();
+//   });
+// });
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".initially-showing").toggle();
-    $(".initially-hidden").toggle();
-    $(".lamprey").toggle();
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
 });
